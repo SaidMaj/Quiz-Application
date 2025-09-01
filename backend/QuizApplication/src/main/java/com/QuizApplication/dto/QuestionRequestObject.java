71be9770d2b2c86d3435405a624367b59addf7c5
SaidMaj <saidalmamri9999@gmail.com>
2025-09-01 07:56:11 +0400

    I have crated a dto package it includes two classes QuestionRequest and QuestionUpdate, and I modified the application.properties

---
+++ backend/QuizApplication/src/main/java/com/QuizApplication/dto/QuestionRequestObject.java
@@ -0,0 +1,22 @@
+package com.QuizApplication.dto;
+
+import com.QuizApplication.entities.difficulty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class QuestionRequestObject {
+    private long id;
+
+    private String category;
+    private difficulty difficultyLevel;
+    private String option1;
+    private String option2;
+    private String option3;
+    private String option4;
+    private String questionTitle;
+    private String rightAnswer;
+}
