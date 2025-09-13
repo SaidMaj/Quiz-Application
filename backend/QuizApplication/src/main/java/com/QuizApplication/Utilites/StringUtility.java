04ad346470a4cea2998d909b9f22c2d1a44f1cfb
SaidMaj <saidalmamri9999@gmail.com>
2025-09-13 16:23:31 +0400

    feat: add StringUtility class with capitalizeWord method

---
+++ backend/QuizApplication/src/main/java/com/QuizApplication/Utilites/StringUtility.java
@@ -0,0 +1,20 @@
+package com.QuizApplication.Utilites;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public final class StringUtility
+{
+    //This method used to Capitalize The String for Example SAID will be converted to Said
+    public static String capitalizeWord(String word)
+    {
+        /*
+            Let's Suppose That I got user Input jAVA
+             j it will be converted to upper
+             AVA it will be converted lower
+             so the final result Java
+        */
+        word = word.substring(0,1).toUpperCase() + word.substring(1).toLowerCase();
+        return word;
+    }
+}
