8d694f60beb162ecf2eeef42765ebceaa643b2ef
SaidMaj <saidalmamri9999@gmail.com>
2025-09-17 11:22:11 +0400

    feat(exceptions): add QuestionNotFoundException

--- backend/QuizApplication/src/main/java/com/QuizApplication/entities/difficulty.java
+++ backend/QuizApplication/src/main/java/com/QuizApplication/entities/difficulty.java
@@ -4,4 +4,5 @@ public enum difficulty {
     Easy,
     Medium,
     Hard
+
 }
--- backend/QuizApplication/src/main/java/com/QuizApplication/services/QuestionService.java
+++ backend/QuizApplication/src/main/java/com/QuizApplication/services/QuestionService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,15 +25,6 @@ public class QuestionService {
 
     public List<Question> getQuestions()
     {
-//        //I have crated this list to store the questions
-//        List <Question> questions = new ArrayList<>();
-//
-//        //this Range-loop loops through all the questions and add question one by one
-//        for (Question question : questionRepository.findAll())
-//        {
-//            questions.add(question);
-//        }
-
         // returns all the questions
         return questionRepository.findAll();
     }
@@ -49,7 +41,7 @@ public class QuestionService {
     public Question editQuestion(QuestionUpdateRequest updateRequest, long questionId) {
 
         //Here I used an method from the repo to get me a specific question by using the questionId
-        Question question = questionRepository.getById(questionId).orElse;
+        Question question = questionRepository.getById(questionId);
 
         //Here the first DTO converted to question Service, after that it saved to the database
         return questionRepository.save(mappingServices.convertDTOToQuestionObject(updateRequest, question));
