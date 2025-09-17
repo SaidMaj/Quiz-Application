3ba39e17c1373ff90e030dc3549b15c03bc4dd21
SaidMaj <saidalmamri9999@gmail.com>
2025-09-17 16:17:26 +0400

    refactor(dto): simplify and unify validation rules in QuestionRequestObject and QuestionUpdateRequest

--- backend/QuizApplication/src/main/java/com/QuizApplication/validation/DifficultyLevelValidator.java
+++ backend/QuizApplication/src/main/java/com/QuizApplication/validation/DifficultyLevelValidator.java
@@ -2,6 +2,8 @@ package com.QuizApplication.validation;
 import com.QuizApplication.entities.difficulty;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import jakarta.validation.constraints.Null;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +18,12 @@ public class DifficultyLevelValidator implements ConstraintValidator<DifficultyL
 
     @Override
     public boolean isValid(String difficultyLevel, ConstraintValidatorContext constraintValidatorContext) {
+
+        //In case the difficultyLevel is null or empty or blank it will return false
+        if(difficultyLevel.isBlank())
+        {
+            return false;
+        }
 
         //Here I have declared an Array List
         List<String> levels = new ArrayList<String>();
